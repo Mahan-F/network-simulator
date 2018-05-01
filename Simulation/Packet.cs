d0836f43d1f8c59a318bb4dda5ccd3fae47cefbc
Mahan Fathi <mahan_f@blasticdesigns.com>
2018-05-01 20:36:23 +0300

    Changed packets class to a struct to enable copying

--- Simulation/Packet.cs
+++ Simulation/Packet.cs
@@ -4,43 +4,13 @@ using System.Text;
 
 namespace Simulation
 {
-    class Packet
+    public struct Packet
     {
-        private string _content;
-        private string _location;
-        private string _destination;
-        private int _ttl;
-        private int _timesTransferred;
-
-        public string Content
-        {
-            get { return _content; }
-            set { _content = value; }
-        }
-
-        public string Location
-        {
-            get { return _location; }
-            set { _location = value; }
-        }
-
-        public string Destination
-        {
-            get { return _destination; }
-            set { _destination = value; }
-        }
-
-        public int TTL
-        {
-            get { return _ttl; }
-            set { _ttl = value; }
-        }
-
-        public int TimesTransferred
-        {
-            get { return _timesTransferred; }
-            set { _timesTransferred = value; }
-        }
+        public string Content;
+        public string Location;
+        public string Destination;
+        public int TTL;
+        public int TimesTransferred;
 
         // Constructor
         public Packet(string _content, string _location, string _destination, int _ttl)
